2f5951eff6709bc97235265d4588f4e27602f093
noahjredshaw <noah.redshaw@my.jcu.edu.au>
2026-09-22 20:28:53 +1000

    Add functions and additional functionality to score checking and temperature conversion programs in prac_02

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -2,23 +2,31 @@
 CP1404/CP5632 - Practical
 Program for temperature conversion
 """
-
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
+
+def main():
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            print(f"Result: {convert_celsius_to_fahrenheit(celsius):.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            print(f"Result: {convert_fahrenheit_to_celsius(fahrenheit):.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+main()
