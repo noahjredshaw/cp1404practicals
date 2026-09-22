05144072aa6a61691eb3fe3086a0510242aa5d8c
noahjredshaw <noah.redshaw@my.jcu.edu.au>
2026-09-22 18:39:21 +1000

    Refactor Password Check program to introduce functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,18 @@
-minimum_password_length = 10
-password = input("Password: ")
-while len(password) < minimum_password_length:
-    print("Password does not meet minimum requirement.")
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def get_password():
     password = input("Password: ")
-print(len(password) * "*")
+    while len(password) < 10:
+        print("Password does not meet minimum requirement.")
+        password = input("Password: ")
+    return password
+
+
+def print_asterisks(password):
+    print(len(password) * "*")
+
+
+main()
